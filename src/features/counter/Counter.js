4e1d9d964cf9a9269840d235c2df2371b780626a
Fabio Quintal <fabioamquintal@gmail.com>
2022-04-12 15:12:08 -0300

    restored

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -14,7 +14,7 @@ export function Counter() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
-  console.log(count)
+
   const incrementValue = Number(incrementAmount) || 0;
 
   return (
