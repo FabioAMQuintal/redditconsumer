619c77aa40ccea53c8e82eed3c837e128b43755a
Fabio Quintal <fabioamquintal@gmail.com>
2022-04-13 11:20:37 -0300

    app structured before updating to react 18

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import redditSlice from './redditSlice'
+import subRedditSlice from './subRedditSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    reddit: redditSlice,
+    subReddit: subRedditSlice
   },
 });
